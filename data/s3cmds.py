79ca46b2a0903932e48e7b7c2d25b32d35d628ab
Andrew MacFie <amacfie@sent.com>
2020-09-07 13:28:17 -0400

    Fix s3cmds

--- data/s3cmds.py
+++ data/s3cmds.py
@@ -3,14 +3,16 @@ import xml.etree.ElementTree as ET
 
 max_lines = int(sys.argv[1])
 
+def yyyymm(yymm):
+    return '19' + yymm if yymm[0] == '9' else '20' + yymm
+
 tree = ET.parse('arXiv_src_manifest.xml')
 root = tree.getroot()
 files = sorted(
     [ { 'yymm': el[-1].text, 'filename': el[1].text } for el in root[:-1] ],
-    key=lambda f: f['yymm'],
+    key=lambda f: yyyymm(f['yymm']),
     reverse=True,
 )
-files = [ f for f in files if f['yymm'][0] != '9' ]
 num_lines = 0
 for f in files:
     if num_lines >= max_lines:
