e47bf930e9420566dd4c5b7a5fb3c34a7d067ff8
Andrew MacFie <amacfie@sent.com>
2020-09-29 11:57:42 -0400

    Fix slow rg for long arg lists

--- info_retrieval/search.sh
+++ info_retrieval/search.sh
@@ -2,25 +2,31 @@
 
 # usage:
 # MATHTEXT_Q1='first query' MATHTEXT_Q2='second query' ./search.sh
-# MATHTEXT_Q1 goes to csearch, MATHTEXT_Q2 goes to grep on results from csearch
-# set MATHTEXT_Q1='' to disable csearch
+# MATHTEXT_Q1 goes to csearch, MATHTEXT_Q2 goes to ag on results from csearch.
+# uses `data/documents_no_newline/`.
 
 set -e
 
 read -r -e -i "$MATHTEXT_Q1" -p "csearch query (MATHTEXT_Q1): " MATHTEXT_Q1
-read -r -e -i "$MATHTEXT_Q2" -p "grep query (MATHTEXT_Q2): " MATHTEXT_Q2
+read -r -e -i "$MATHTEXT_Q2" -p "ag query (MATHTEXT_Q2): " MATHTEXT_Q2
 
 # https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself#comment54598418_246128
 dirpath="$(dirname "$(readlink -f "$0")")"
 results_file=$(mktemp)
 temp_file=$(mktemp)
-if [[ -z "$MATHTEXT_Q1" ]]; then
-  rg -l --multiline --pcre2 "$MATHTEXT_Q2" "${dirpath}/../data/documents" > $results_file
+if [[ -z "$MATHTEXT_Q1" ]] && [[ -z "$MATHTEXT_Q2" ]]; then
+  exit 1
+elif [[ -z "$MATHTEXT_Q1" ]]; then
+  rg --multiline --pcre2 -l "$MATHTEXT_Q2" "${dirpath}/../data/documents_no_newline" > $results_file
+elif [[ -z "$MATHTEXT_Q2" ]]; then
+  csearch -l "$MATHTEXT_Q1" > ${results_file}
 else
   csearch -l "$MATHTEXT_Q1" > ${temp_file}
   if [[ -s ${temp_file} ]]; then
+    # rg has been slow for long command lines
     # https://unix.stackexchange.com/a/494689
-    xargs -d '\n' -a ${temp_file} rg -l --multiline --pcre2 "$MATHTEXT_Q2" > $results_file
+    # yes if there are multiple ag calls `$results_file` has output from all
+    xargs -d '\n' -a ${temp_file} ag -l "$MATHTEXT_Q2" > $results_file
   else
     echo -n "" > $results_file
   fi
