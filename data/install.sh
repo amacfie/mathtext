59be9d404bef5e2c1176bed389a7b829fd1231cd
Andrew <amacfie@sent.com>
2021-03-12 13:40:13 -0500

    Update install.sh

--- data/install.sh
+++ data/install.sh
@@ -2,7 +2,7 @@
 sudo apt update
 sudo apt install s3cmd p7zip-full parallel pv xml-twig-tools
 pip3 install wheel
-pip3 install unp beautifulsoup4 unidecode tqdm psutil
+pip3 install unp beautifulsoup4 unidecode tqdm psutil lxml
 if [ -z "$(s3cmd --dump-config | grep -P '^access_key' | cut -d' ' -f3)" ]
 then
   s3cmd --configure
